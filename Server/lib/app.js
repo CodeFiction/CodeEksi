e8e25b257791927ba071a3e009f9421f5d5950bf
msusur <mail@mertsusur.com>
2016-02-19 23:26:00 +0200

    initial web project created.

---
+++ Server/lib/app.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    "use strict";
+    var app = angular.module("codeSozluk", []);
+    app.controller("home", function($scope, $http) {
+        var init = function() {
+            //$http.get("");
+        };
+    });
+})(angular);
