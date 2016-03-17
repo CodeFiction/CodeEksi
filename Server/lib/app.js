d63da42d93ff4f7074d7cf408116b4e8549c7c89
msusur <mail@mertsusur.com>
2016-03-17 22:50:36 +0200

    storage completed.

--- Server/lib/app.js
+++ Server/lib/app.js
@@ -11,6 +11,18 @@
             "search": "titles/search",
             "titles": "titles"
         })
+        .factory("storage", [function () {
+            return function () {
+                return {
+                    read: function (key) {
+                        return localStorage.getItem(key);
+                    },
+                    write: function (key, value) {
+                        return localStorage.setItem(key, value);
+                    }
+                }
+            }();
+        }])
         .config(["$routeProvider", "cfpLoadingBarProvider", function ($routeProvider, cfpLoadingBarProvider) {
             $routeProvider.when("/", {
                 templateUrl: "home/popular",
@@ -49,8 +61,13 @@
                 });
             };
         })
-        .controller("detailController", function ($rootScope, $scope, $http, $routeParams, apiConfig) {
+        .controller("detailController", ["$rootScope", "$scope", "$http", "$routeParams", "apiConfig", "storage", function ($rootScope, $scope, $http, $routeParams, apiConfig, storage) {
             window.scrollTo(0, 0);
+            $rootScope.okay = storage.read("theme") === "true";
+            $rootScope.$watch("okay", function (n, o) {
+                storage.write("theme", n);
+            });
+
             $scope.title = {
                 title: $routeParams.title
             };
@@ -83,20 +100,24 @@
                     $scope.errorModels = result;
                 }
             });
-        })
-        .controller("homeController", function ($scope, $http, $window, apiConfig) {
+        }])
+        .controller("homeController", ["$scope", "$http", "$window", "apiConfig", "storage", "$rootScope", function ($scope, $http, $window, apiConfig, storage, $rootScope) {
             $scope.entries = [];
+            $rootScope.okay = storage.read("theme") === "true";
+            $rootScope.$watch("okay", function (n, o) {
+                storage.write("theme", n);
+            });
             $http.get(apiConfig.baseUrl + apiConfig.debe).success(function (result) {
                 $scope.entries = result.debe_title_models;
                 setTimeout(window.refreshNumberOfline, 100);
             });
-        });
+        }]);
 })(angular, $);
 
 (function ($) {
     function calcNumberOfLine() {
         var fontHeight = parseInt($("body").css('line-height'));
-        return parseInt($("body").height() / fontHeight)-20;
+        return parseInt($("body").height() / fontHeight) - 20;
     };
     function appendLine(number) {
         var $li = $("<li>");
